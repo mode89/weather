a7b691220ae87dae236c6d72de4cb02f2e7b4921
Mode <mode89@mail.ru>
2016-03-24 22:59:58 +0800

    add list of input parameters

--- weather.py
+++ weather.py
@@ -1,4 +1,16 @@
 import data
 
+INPUT_PARAMETERS = [
+    "temp",
+    "slp",
+    "vis",
+    "wind",
+    "wind_max",
+    "temp_max",
+    "temp_min",
+    "fog",
+    "rain"
+]
+
 if __name__ == "__main__" :
     stations = data.load("data.txt")
