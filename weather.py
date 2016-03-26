903a103a4cfb4f0c7d02e173f8d6ddfd3fc321b5
Mode <mode89@mail.ru>
2016-03-26 13:59:35 +0800

    check missing data values

--- weather.py
+++ weather.py
@@ -4,9 +4,8 @@ import data
 START_DATE = date(1973, 3, 1)
 END_DATE = date(1974, 9, 30)
 
-INPUT_PARAMETERS = [
+PARAMETERS = [
     "temp",
-    "slp",
     "vis",
     "wind",
     "wind_max",
@@ -28,10 +27,21 @@ if __name__ == "__main__" :
     print("Checking data...")
     fmissdays = open("missdays.txt", "w")
     missdays_cnt = 0
+    fmissvals = open("missvals.txt", "w")
+    missvals_cnt = 0
     for day in daterange(START_DATE, END_DATE):
         for s in stations:
             if day not in stations[s]:
                 fmissdays.write("{0} {1}\n".format(s, day))
                 missdays_cnt += 1
+            else:
+                record = stations[s][day]
+                for p in PARAMETERS:
+                    if getattr(record, p) is None:
+                        fmissvals.write("{0} {1} {2}\n".format(s, day, p))
+                        missvals_cnt += 1
     fmissdays.close()
+    fmissvals.close()
     print("{0} missing records".format(missdays_cnt))
+    print("{0} missing values".format(missvals_cnt))
+    print("{0} parameters".format(len(PARAMETERS)))
